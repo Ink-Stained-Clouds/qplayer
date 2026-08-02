9e06dc1e521110dddef1341f832cc803183dd2b7
Ink-Stained-Clouds <inkclouds314419@gmail.com>
2026-08-02 12:57:59 +0800

    chore: bump version to 0.9.3

--- android-shell/app/build.gradle.kts
+++ android-shell/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "dev.t1m3.qplayer"
         minSdk = 26
         targetSdk = 34
-        versionCode = 53
-        versionName = "0.9.2"
+        versionCode = 54
+        versionName = "0.9.3"
         manifestPlaceholders["appLabel"] = "QPlayer"
     }
 
