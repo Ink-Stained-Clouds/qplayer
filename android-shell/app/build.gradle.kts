d43132bb4785b1a672b05494ff8424b1343d009b
TIMER-err <zqq2333@outlook.com>
2026-08-07 22:20:12 +0800

    chore: bump version to 0.9.6

--- android-shell/app/build.gradle.kts
+++ android-shell/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "dev.t1m3.qplayer"
         minSdk = 26
         targetSdk = 34
-        versionCode = 56
-        versionName = "0.9.5"
+        versionCode = 57
+        versionName = "0.9.6"
         manifestPlaceholders["appLabel"] = "QPlayer"
     }
 
