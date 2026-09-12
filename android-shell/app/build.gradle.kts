193025856ae931ea83263e9281b86acd83b5f119
Ink-Stained-Clouds <inkclouds314419@gmail.com>
2026-09-12 12:05:51 +0800

    chore: 发布 1.5.2

--- android-shell/app/build.gradle.kts
+++ android-shell/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "dev.t1m3.qplayer"
         minSdk = 26
         targetSdk = 34
-        versionCode = 73
-        versionName = "1.5.1"
+        versionCode = 74
+        versionName = "1.5.2"
         manifestPlaceholders["appLabel"] = "QPlayer"
     }
 
